ebef1e18b286d8bae66a62e4c07d4d31d3c201c6
tutaia32 <tutaiauejso@gmail.com>
2025-12-02 22:01:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "8377048941:AAFToOCirkuyttcPmlQQS2--VpA78YiuBvM,
+  BOT_TOKEN: "8377048941:AAFToOCirkuyttcPmlQQS2--VpA78YiuBvM",
   OWNER_ID: "5894696119",
   // Konfigurasi server sekarang ada di src/database/servers.json
 };
